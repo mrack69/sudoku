17a2fdcf7d79daabdc0517d502cbce934099e18a
Dmitry Klyushentsev <klyushentsev@gmail.com>
2021-07-14 12:57:03 +0300

    feat: add func witch DFS

--- src/index.js
+++ src/index.js
@@ -1,120 +1,65 @@
 module.exports = function solveSudoku(matrix) {
-  //Найти уникальный возможный элемент среди возможный в строке, в столбце или ячейке и забисать его.
-  let result = stepMatrix(matrix);
-  console.log(result);
-  return result;
-}
-
-function stepMatrix(matrix) {
-  let nullStore = [];
-  matrix.forEach((line, i) => {
-    line.forEach((el, index) => {
-      if (el === 0) nullStore.push({
-        x: index,
-        y: i,
-      });
-    });
-  });
-  //Найти возможные значения для строки, для столбца, для 3х3 а потом положить в объект пересечение 3 массивов.
-  let countEdit = 0;
-  nullStore.forEach((nullObj, index) => {
-    const possibleValuesInRow = [];
-    const possibleValuesInCol = [];
-    const possibleValuesIn3x3 = [];
-    const row = getRow(matrix, nullObj.y);
-    const col = getCol(matrix, nullObj.x);
-    const this3x3 = get3x3(matrix, nullObj.x, nullObj.y)
-    for (let i = 1; i < 10; i++) {
-      if (!row.includes(i)) possibleValuesInRow.push(i);
-      if (!col.includes(i)) possibleValuesInCol.push(i);
-      if (!this3x3.includes(i)) possibleValuesIn3x3.push(i);
-    }
-    let possibleValues = possibleValuesInRow.filter(x => possibleValuesInCol.includes(x)).filter(x => possibleValuesIn3x3.includes(x));
-    if(possibleValues.length === 1) {
-      matrix[nullObj.y][nullObj.x] = possibleValues[0];
-      countEdit++;
-    }
-    else {
-      nullStore[index].possibleValues = possibleValues;
-      nullStore[index].sectionId = get3x3Id(nullObj.x, nullObj.y)
-    }
-  });
-  nullStore = nullStore.filter(nullObj => nullObj.possibleValues);
-  if(countEdit) matrix = stepMatrix(matrix);
-  else matrix = setUniqPossValue(matrix, nullStore);
-  return matrix;
-}
-
-function setUniqPossValue(matrix, nullStore) {
-  let countEdit = 0;
-  for(let nullItem of nullStore) {
-    for(let value of nullItem.possibleValues) {
-      //Находим все возможные значения в линии
-      let allPossValueInLine = nullStore.filter(nullObj => nullObj.x === nullItem.x);
-      allPossValueInLine = allPossValueInLine.reduce((res, el) => {res.push(el.possibleValues); return res}, []).flat();
-      const countValueInLine = allPossValueInLine.filter(el => el === value).length;
-      if(countValueInLine === 1) {
-        matrix[nullItem.y][nullItem.x] = value;
-        countEdit++;
-        break;
-      }
-      //Находим все возможные значения в колонке
-      let allPossValueInCollumn = nullStore.filter(nullObj => nullObj.y === nullItem.y);
-      allPossValueInCollumn = allPossValueInCollumn.reduce((res, el) => {res.push(el.possibleValues); return res}, []).flat();
-      const countValueInCollumn = allPossValueInCollumn.filter(el => el === value).length;
-      if(countValueInCollumn === 1) {
-        matrix[nullItem.y][nullItem.x] = value;
-        countEdit++;
-        break;
-      }
-      //Находим все возможные значения в секции
-      let allPossValueInSection = nullStore.filter(nullObj => nullObj.sectionId === nullItem.sectionId);
-      allPossValueInSection = allPossValueInSection.reduce((res, el) => {res.push(el.possibleValues); return res}, []).flat();
-      const countValueInSection = allPossValueInSection.filter(el => el === value).length;
-      if(countValueInSection === 1) {
-        matrix[nullItem.y][nullItem.x] = value;
-        countEdit++;
-        break;
+  //Функция нахождения нуля
+  function findNull(matrix) {
+    for(let y = 0; y < 9; y++) {
+      for(let x = 0; x < 9; x++) {
+        if(matrix[y][x] === 0) {
+          return [x, y];
+        }
       }
     }
+    return null;
   }
-  if(countEdit) matrix = stepMatrix(matrix);
+
+  function getCollumn(matrix, x) {
+    let result = [];
+    matrix.forEach(row => {
+      result.push(row[x])
+    });
+    return result;
+  }
+
+  function getSection(matrix, x, y) {
+    const startX = Math.floor(x / 3) * 3;
+    const startY = Math.floor(y / 3) * 3;
+    const result = [];
+    for(let y = startY; y < startY + 3; y++) {
+      for(let x = startX; x < startX + 3; x++) {
+        result.push(matrix[y][x]);
+      }
+    }
+    return result;
+  }
+
+  function isValid(matrix, currentPos, currentElem) {
+    const [x, y] = currentPos;
+    const row = matrix[y];
+    const col = getCollumn(matrix, x);
+    const section = getSection(matrix, x, y);
+    if(row.includes(currentElem) || col.includes(currentElem) || section.includes(currentElem)){
+      return false;
+    }
+    return true;
+  }
+
+  function solve() {
+    const currentPos = findNull(matrix);
+    if(currentPos === null) return true;
+    const [x, y] = currentPos;
+    for(let i = 1; i < 10; i++) {
+      if(isValid(matrix, currentPos, i)) {
+        const [x, y] = currentPos;
+        matrix[y][x] = i;
+        if(solve()) return true;
+        matrix[y][x] = 0;
+      }
+    }
+    return false;
+  }
+  solve();
+  console.log(matrix);
   return matrix;
 }
 
-function getRow(matrix, y) {
-  return matrix[y];
-}
 
-function getCol(matrix, x) {
-  const col = [];
-  matrix.forEach(line => col.push(line[x]));
-  return col;
-}
 
-function get3x3(matrix, x, y) {
-  const result =[];
-  let lineXx3;
-  let lineYx3;
-  if (x < 3) lineXx3 = 0;
-  else if (x < 6) lineXx3 = 3;
-  else lineXx3 = 6;
-  if (y < 3) lineYx3 = 0;
-  else if (y < 6) lineYx3 = 3;
-  else lineYx3 = 6;
-  const lines = matrix.slice(lineYx3, lineYx3 + 3);
-  lines.forEach(line => result.push(line.slice(lineXx3, lineXx3 + 3)));
-  return result.flat();
-}
-
-function get3x3Id(x, y) {
-  if(x < 3 && y < 3) return 1;
-  else if(x < 6 && y < 3) return 2;
-  else if(x < 9 && y < 3) return 3;
-  if(x < 3 && y < 6) return 4;
-  else if(x < 6 && y < 6) return 5;
-  else if(x < 9 && y < 6) return 6;
-  if(x < 3 && y < 9) return 7;
-  else if(x < 6 && y < 9) return 8;
-  else if(x < 9 && y < 9) return 9;
-}
